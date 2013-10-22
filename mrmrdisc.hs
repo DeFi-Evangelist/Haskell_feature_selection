a0b49f6f647f23cd140958cbe479ec0344310718
Aleksei <alex2-92@mail.ru>
2013-10-22 22:45:49 +0200

    Update mrmrdisc.hs

--- mrmrdisc.hs
+++ mrmrdisc.hs
@@ -9,7 +9,6 @@
                         -- "Brain and Trauma Foundation" Switzerland
 
 -- ******************************************************************************/
-
 import Data.List
 import Data.Ord
 
@@ -19,7 +18,7 @@ sort_stro (_, m1) (_, m2)
   | m1 > m2 = LT
   | m1 == m2 = EQ
  
-maxtri :: ((Float, Float, Float), [(a, Float)]) ->
+maxtri :: Eq a => ((Float, Float, Float), [(a, Float)]) ->
 	((Float, Float, Float), [(a, Float)]) -> ((Float, Float, Float), [(a, Float)])
 maxtri ((d1, r1, s1), st1) ((d2, r2, s2), st2)
 	| oldlsnw == True = ((d2, r2, s2), st2)
@@ -35,7 +34,7 @@ oldlsnwf ((d1, r1, s1), _) ((d2, r2, s2), _) = ((d1/s1) + (r1/(s1^2))) < ((d2/s2
 (/=/) :: Eq a => (a, a, Float) -> (a, Float) -> Bool
 (/=/) (li, lk, _) (oj, _) = (li /= oj) && (lk /= oj)
 
-mrmri :: ((Float, Float, Float), [(a, Float)]) -> [(a, a, Float)] -> 
+mrmri :: Eq a =>((Float, Float, Float), [(a, Float)]) -> [(a, a, Float)] -> 
 	(a, Float) -> ((Float, Float, Float), [(a, Float)])
 mrmri ((d, r, s), setf) lf (oxi, doi) = ((d2, r2, s2), setf2)
 	where 
@@ -44,7 +43,7 @@ mrmri ((d, r, s), setf) lf (oxi, doi) = ((d2, r2, s2), setf2)
 		s2 = s + 1
 		setf2 = ((oxi, doi):setf)
 		
-mrmrn3 :: ((Float, Float, Float), [(a, Float)]) 
+mrmrn3 :: Eq a =>((Float, Float, Float), [(a, Float)]) 
 	-> [(a, a, Float)] -> [(a, Float)] -> ((Float, Float, Float), [(a, Float)])
 mrmrn3 ((d, r, s), setf) lf (ox:oxs) = if oldlsnw
 	then maxtri thismr (mrmrn3 ((d, r, s), setf) (lf/\ox) oxs)
@@ -54,19 +53,20 @@ mrmrn3 ((d, r, s), setf) lf (ox:oxs) = if oldlsnw
 		oldlsnw = oldlsnwf ((d, r, s), setf) thismr
 mrmrn3 ((d, r, s), setf) _ [] = ((d, r, s), setf)
 
-mutifl :: Ord a => ([a] -> [a] -> Float) -> [[a]] -> [(a, a, Float)]
+mutifl :: Ord a => ([a] -> [a] -> Float) -> [[a]] -> [(Int, Int, Float)]
 mutifl f dtlst = [(i, k, (f (dtlst!!fromIntegral(i)) (dtlst!!fromIntegral(k)))) | i <- [0..n], k <- [0..n], i<k]
 	where n = length dtlst
 
-mutiol :: Ord a => ([a] -> [a] -> Float) -> [[a]] -> [a] -> a -> [(a, Float)]
-mutiol f (x:xs) outs n = [(n, f x outs):(mutiol f xs outs (n+1))]
+mutiol :: (Ord a , Num a) => ([a] -> [a] -> Float) -> [[a]] -> [a] -> a -> [(a, Float)]
 mutiol _ [] _ _ = []
+mutiol f (x:xs) outs n = (n, f x outs):(mutiol f xs outs (n+1))
 
-mrmrdiscr :: Ord a => ([a] -> [a] -> Float) -> [[a]] -> [(a, Float)]
-mrmrdiscr f dtlst outs = mrmrn3 ((z, z, z), setf) lf oxoxs
+mrmrdiscr ::(Ord a , Num a)=> ([a] -> [a] -> Float) -> [[a]]-> [a] -> [(Int, Float)]
+mrmrdiscr f dtlst outs = setout
 	where 
 		mutiolsort = sortBy sort_stro (mutiol f dtlst outs 0)
 		setf = take 2 mutiolsort
 		oxoxs = drop 2 mutiolsort
 		lf = ((mutifl f dtlst)/\(setf!!0))/\(setf!!1)
 		z = fromIntegral(0) :: Float
+		setout = snd ( mrmrn3 ((z, z, z), setf) lf oxoxs)
